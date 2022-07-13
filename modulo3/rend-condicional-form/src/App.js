e930ad181074bce3d14fb8b5634d762531446679
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-07-12 22:11:28 -0300

    formulario teste feito

--- modulo3/rend-condicional-form/src/App.js
+++ modulo3/rend-condicional-form/src/App.js
@@ -1,8 +1,33 @@
+import React from 'react'
+import {DivFormulario} from './style'
+
 
 function App() {
   return (
-    <>
-    </>
+    <DivFormulario>
+      <h1>Etapa 1 - Dados Gerais</h1>
+      <label>
+        Nome:
+        <input type="text" placeholder="Insira seu nome"/>
+      </label>
+      <label>
+        Idade:
+        <input type="text" placeholder="Insira sua idade"/>
+      </label>
+      <label>
+        Email:
+        <input type="text" placeholder="Insira seu email"/>
+      </label>
+      <label>Grau de escolaridade:
+      <select>
+        <option value="EnsinoMedioIncompleto">Ensino Médio Incompleto</option>
+        <option value="EnsinoMedioCompleto">Ensino Médio Completo</option>
+        <option value="EnsinoSuperiorIncompleto">Ensino Superior Incompleto</option>
+        <option value="EnsinoSuperiorIncompleto">Ensino Superior Completo</option>
+      </select>
+      </label>
+      <input type="submit" value="Enviar"/>
+    </DivFormulario>
   );
 }
 
--- modulo3/rend-condicional-form/src/style.js
+++ modulo3/rend-condicional-form/src/style.js
@@ -1,1 +1,24 @@
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const DivFormulario = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #1fbaa5;
+  height: 40vh;
+  width: 30vw;
+  margin-top: 15vh;
+  border-radius: 15px;
+
+  h1{
+    font-size: 23px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  label{
+    margin: 10px;
+  }
+
+`;

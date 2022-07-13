a3c38b7f2d3d43144039c446bbc3a784592108dd
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-07-13 05:43:19 -0300

    desafio

--- modulo3/rend-condicional-form/src/components/Agradecimentos.js
+++ modulo3/rend-condicional-form/src/components/Agradecimentos.js
@@ -1,45 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 import {DivAgradecimentos} from '../style'
-import DadosGerais from "./DadosGerais";
-import CursoSuperiorIncompleto from "./CursoSuperiorIncompleto";
 
 function Agradecimentos (props) {
 
   const tituloEncerramento = "O formulário acabou!";
 
-  const [nav, setNav] = useState("")
-
-  const handleCursoSuperiorIncompleto = () => {
-    setNav("Voltar")
-  }
-
-  const handleDadosGerais = ()=> {
-    setNav("Finalizar")
-  }
-
-  switch(nav) {
-    case "Voltar":
-      return <CursoSuperiorIncompleto/>
-      break;
-    case "Finalizar":
-      return <DadosGerais/>
-      break;
-    default:
-      break;
-  }
-
   return (
     <DivAgradecimentos>
       <h1>{tituloEncerramento}</h1>
       <p>Agradecemos sua participação.</p>
-      <div>
-        <span>
-          <button onClick={handleCursoSuperiorIncompleto}>Voltar</button>
-        </span>
-        <span>
-          <button onClick={handleDadosGerais}>Finalizar</button>
-        </span>
-      </div>
     </DivAgradecimentos>
   )
 }

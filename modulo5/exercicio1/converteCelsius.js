34e2ece76f924efef0584acd52faf3b6dfaee6fe
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-13 17:24:08 -0300

    corrigindo a lógica

--- modulo5/exercicio1/converteCelsius.js
+++ modulo5/exercicio1/converteCelsius.js
@@ -1,10 +1,17 @@
 const converteCelsius = (temperatura, escala) => {
+  if (typeof temperatura !== "number") {
+    return "Erro, parâmetro inválido. Corrigir temperatura."
+  }
+  if (typeof escala !== "string") {
+    return "Erro. Parâmetro inválido. Insira uma string para unidade de temperatura."
+  }
+  if (escala !== "F" && escala !== "K") {
+    return "Erro. Parâmetro inválido. Insira uma string para unidade de temperatura."
+  }
   if (escala === "K") {
     return `${temperatura}° Celsius é equivalente a ${temperatura+273} Kelvin`;
   } else if (escala === "F") {
     return `${temperatura}° Celsius é equivalente a ${temperatura*1.8+32} Fahrenheit`;
-  } else {
-    return `Erro. Parâmetro inválido ("nome do parâmetro").`
   }
 }
 

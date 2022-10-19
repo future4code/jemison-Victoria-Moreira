f64ea496e40b099d132710cb42c287a9a37206ed
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-18 23:55:23 -0300

    configuração e primeiro exercicio

---
+++ modulo5/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,13 @@
+function checaTriangulo(a: number, b: number, c: number) : string {
+  if (a !== b && b !== c && c !== a) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
+}
+
+console.log(checaTriangulo(11, 9, 7))
+console.log(checaTriangulo(3, 3, 3))
+console.log(checaTriangulo(3, 8, 3))

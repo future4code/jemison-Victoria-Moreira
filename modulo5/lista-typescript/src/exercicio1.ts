0555bce45bde5711a2a3b4924a3870621ebbac24
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-24 18:38:48 -0300

    exercicio1 com erro

---
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -0,0 +1,13 @@
+
+function separateData (
+    name: string, 
+    birthDate: string
+    ){
+      
+    const data = birthDate.split('/')
+      
+    return `Olá me chamo ${name}, nasci no dia ${data[0]} do mês de ${data[1]} do ano de ${data[2]}.`
+  }
+
+console.log(separateData("Victoria", "12/05/1996"))
+

7e7b9a470604d74bc2aabf5b112f293aecbffa9f
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-24 20:05:50 -0300

    exercicio3 ok

--- modulo5/lista-typescript/src/exercicio1.ts
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -1,3 +1,4 @@
+//Crie um função que receba uma string com o nome e outra string com uma data de nascimento (ex.: “24/04/1993”). A função deve separar o dia, o mês e ano e retornar uma string no seguinte formato: "Olá me chamo {NOME}, nasci no dia {DIA} do mês de {MÊS} do ano de {ANO}" 
 
 function separateData (
     name: string, 
--- modulo5/lista-typescript/src/exercicio2.ts
+++ modulo5/lista-typescript/src/exercicio2.ts
@@ -1,3 +1,5 @@
+//Crie uma função que receba um parâmetro qualquer e que imprima no console o tipo da variável. 
+
 type Bandeira = string | boolean | number | undefined |null
 
 

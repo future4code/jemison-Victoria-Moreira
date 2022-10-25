b1d154400ae054a0fca467fc64ddee49776b20a0
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-24 21:56:24 -0300

    exercicio6 ok

---
+++ modulo5/lista-typescript/src/exercicio6.ts
@@ -0,0 +1,48 @@
+//Agora, pediram para você ajudar a fazer uma funcionalidade de um banco digital. Antes de explicar a sua tarefa, você precisa entender como eles guardam as contas dos clientes. Basicamente, eles salvam o nome do clientes, o saldo total e uma lista contendo todas os débitos realizados pelo cliente. Exemplo:
+
+// entrada
+// [
+// 	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+// ]
+
+// Pensando em aumentar seu lucros, o banco quer identificar possíveis clientes precisando de empréstimos. Dessa forma, a sua tarefa é criar uma função que receba este array como parâmetro, atualize o saldo total descontando todos os débitos e retorne apenas os clientes com saldo negativo.
+
+
+type Clientes = {
+  cliente: string,
+  saldoTotal: number,
+  debitos: Array<number>
+}
+
+const listaDeClientes: Clientes [] = [
+	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+]
+
+function listaDeCredores (lista:Array<Clientes>) {
+  let debitosTotais: number
+  let soma: number
+  let saldo: Clientes[] = []
+  lista.map((item)=>{
+    debitosTotais = item.debitos.reduce((total, atual) => total + atual, 0);
+    soma = item.saldoTotal - debitosTotais;
+    item.saldoTotal = soma;
+    item.debitos = []
+
+    if (soma < 0) {
+      saldo.push(item)
+    }
+  })
+  return saldo
+}
+
+console.log(listaDeCredores(listaDeClientes))

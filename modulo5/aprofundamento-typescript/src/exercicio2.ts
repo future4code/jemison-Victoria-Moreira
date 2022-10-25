e7700f226d6d44c4a446b937c997dae8c4edd62e
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-10-24 22:08:37 -0300

    removendo comentário

--- modulo5/aprofundamento-typescript/src/exercicio2.ts
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -1,30 +1,3 @@
-// function obterEstatisticas(numeros: number[]) {
-
-//     const numerosOrdenados: number[] = numeros.sort(
-//         (a: number, b: number) => a - b
-//     )
-
-//     let soma: number = 0
-
-//     for (let num of numeros) {
-//         soma += num
-//     }
-
-//     type TipoEstatisticas = {
-//         maior : number,
-//         menor: number,
-//         media: number
-//     }
-
-//     const estatisticas: TipoEstatisticas = {
-//         maior: numerosOrdenados[numeros.length - 1],
-//         menor: numerosOrdenados[0],
-//         media: soma / numeros.length
-//     }
-
-//     return estatisticas
-// }
-
 type Amostra = {
   numeros: any,
   obterEstatisticas: any

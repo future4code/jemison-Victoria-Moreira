03705788843b8c41f0f085cb0d5f4f69c33bbf37
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-11-07 14:52:15 -0300

    feat: criando o "banco de dados" no data.ts

---
+++ modulo5/intro-express/src/data.ts
@@ -0,0 +1,36 @@
+//3- Crie um array de usuários para servir como base de dados da nossa API.
+
+import { traceDeprecation } from "process";
+import { User } from "./types";
+
+
+export const users: User[] = [
+    {
+       id: 1,
+       name: "vanessa",
+       phone: "13222444",
+       email: "van.martins@hotmail.com",
+       website: "van-martins.com"
+    },
+    {
+        id: 2,
+        name: "julia",
+        phone: "82400101",
+        email: "jucabral@hotmail.com",
+        website: "jucabral.com"
+     },
+     {
+        id: 3,
+        name: "danielle",
+        phone: "12445657",
+        email: "dani@hotmail.com",
+        website: "dani.com"
+     },
+     {
+        id: 4,
+        name: "tadeu",
+        phone: "24128874"
+        email: "tad.franco@hotmail.com",
+        website: "tad.franco.com"
+     },
+]

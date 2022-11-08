d70b94cd780609c262c6abd32dfcaf8e9033045c
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-11-08 00:02:30 -0300

    feat: iniciando, instalando e configurando express

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 import cors from "cors"
 
@@ -7,6 +7,11 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
+
+//teste
+app.get("/ping",(req:Request,res:Response)=>{
+    res.send("pong")
+});
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");

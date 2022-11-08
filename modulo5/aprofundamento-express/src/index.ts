06c092bc104859c1310b298e61d2c7113e1d222d
Victoria Moreira <vdsmoreira@outlook.com.br>
2022-11-08 00:15:54 -0300

    feat: criando variavel de tipo para cada tarefa

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use(cors())
 
-//teste
+//1- Faça novamente a instalação e configuração do Express na pasta do exercício. Para testar, crie um endpoint que aponte para o path “/ping”. Esse endpoint pode responder apenas com uma mensagem “pong”.
 app.get("/ping",(req:Request,res:Response)=>{
     res.send("pong")
 });
---
+++ modulo5/aprofundamento-express/src/type.ts
@@ -0,0 +1,9 @@
+
+//2- Crie uma variável de tipo para representar cada afazer.
+
+export type ToDoList = {
+  userId: number,
+  id: number,
+  title: "string",
+  completed: boolean
+}

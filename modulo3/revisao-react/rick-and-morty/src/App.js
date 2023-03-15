a7b771dc9b7078bcf32f114fed5266fa0a1a094e
Victoria Moreira <vdsmoreira@outlook.com.br>
2023-03-15 15:31:34 -0300

    feat (components): adds components

--- modulo3/revisao-react/rick-and-morty/src/App.js
+++ modulo3/revisao-react/rick-and-morty/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import { DetailsPage } from './pages/DetailsPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>Rick and Morty</h1>
+      <HomePage/>
+      <DetailsPage/>
     </div>
   );
 }
---
+++ modulo3/revisao-react/rick-and-morty/src/pages/HomePage.js
@@ -0,0 +1,7 @@
+export function HomePage () {
+  return(
+    <>
+      Tela de HomePage
+    </>
+  )  
+}
